e86c06fd3430fb9308be51718ebda1a154266b71
kaida-jyouma <53356184+kaida-jyouma@users.noreply.github.com>
2019-07-30 01:18:17 +0900

    Update how-to-sqrt_or_more.cs

--- how-to-sqrt_or_more.cs
+++ how-to-sqrt_or_more.cs
@@ -1,25 +1,22 @@
 using System;
 public class program{
-	// べき乗を関数化
-	public static double pow(double x, int y){
+	// tofunction "Math.Pow()"
+	public static double pow(double x, double y){
 		return Math.Pow(x, y);
 	}
-	// 開平法 (x => n乗根を求めたい数, y => n);
-	public static double rt(int x, int y){
+	// sqrt
+	public static double rt(int x, int y = 2){
 		var num = x;
 		double prm = 0;
 		for (var i=0;i<16;i++){
 			double ct = pow(10, -(i));
-			while (pow(prm, y) < num){
-				prm += ct;
-			}
+			while (pow(prm, y) < num) prm += ct;
 			prm -= ct;
 		}
 		return prm;
 	}
 	// 出力
 	public static void Main(){
-		//入力値は "2 3" <=2の3乗を求める
 		var j = Console.ReadLine();
 		Console.WriteLine(" in: " + j);
 		string[] nums = j.Split(' ');
