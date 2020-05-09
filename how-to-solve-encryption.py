55447359a18033f39b89c0d64721c8ce48cf4aec
kaida-jyouma <53356184+kaida-jyouma@users.noreply.github.com>
2020-05-09 20:45:34 +0900

    Update how-to-solve-encryption.py

--- how-to-solve-encryption.py
+++ how-to-solve-encryption.py
@@ -1,4 +1,4 @@
-"""暗号化ツールplus_ver2.0.9"""
+"""暗号化ツールplus_ver4.5.3"""
 import urllib.parse as urp
 # rot13
 def rot13(x):
@@ -60,8 +60,6 @@ def fix10_2(x):
             if 2 ** n > x:
                 break
         return("".join([str(k) for k in l]))
-# 10進数→3進数
-# def 
 # 10進数→36進数
 def fix10_36(x):
     dgt = 0
@@ -227,80 +225,6 @@ def solvengm(string, keydate, rot=0):
     for i in range(len(ansls)):
         ret.append(alpb[ansls[i]])
     return("".join(ret))
-# 強化版エニグマもどき
-def engmpow(string, key=False, rot=0):
-    import datetime, time
-    now = str(datetime.datetime.now(datetime.timezone(datetime.timedelta(hours=9))).strftime('%y.%m.%d-%H.%M.%S'))
-    nout = now + "." + str(time.time()).split(".")[1]
-    text = [c for c in nout]
-    for i in range(text.count(".")):
-        text.remove(".")
-    noutim = "".join("".join(text).split())
-    ct0 = ct1 = ct2 = ct3 = ct4 = ct5 = ct6 = 0
-    for i in list(map(int, [c for c in noutim])):
-        if i % 7 == 0:
-            ct0 += 1
-        elif i % 7 == 1:
-            ct1 += 1
-        elif i % 7 == 2:
-            ct2 += 1
-        elif i % 7 == 3:
-            ct3 += 1
-        elif i % 7 == 4:
-            ct4 += 1
-        elif i % 7 == 5:
-            ct5 += 1
-        elif i % 7 == 6:
-            ct6 += 1
-    lstr = [c for c in string]
-    alpb = [chr(c) for c in range(97, 123)]
-    alpb.extend(list(map(chr, list(range(65, 91)))))
-    alpb.extend(list(map(chr, [32, 33, 44, 45, 46, 47, 58, 61, 63, 95])))
-    alpb.extend(list(map(chr, list(range(48, 58)))))
-    if rot != 0:
-        als1 = alpb[-rot:]
-        als1.extend(alpb[:len(alpb) - rot])
-        alpb = als1
-    ansls = []
-    for i in range(len(lstr)):
-        if i % 7 == 0:
-            ind = alpb.index(lstr[i]) - ct0 + ct1 - ct2 + ct3 - ct4 + ct5 - ct6
-            if ind < 0:
-                ind += len(alpb)
-        elif i % 7 == 1:
-            ind = alpb.index(lstr[i]) + ct1 - ct2 + ct3 - ct4 + ct5 - ct6 + ct0
-            if ind >= len(alpb):
-                ind -= len(alpb)
-        elif i % 7 == 2:
-            ind = alpb.index(lstr[i])
-            if ind < 0:
-                ind += len(alpb)
-        elif i % 7 == 3:
-            ind = alpb.index(lstr[i])
-            if ind >= len(alpb):
-                ind -= len(alpb)
-        elif i % 7 == 4:
-            ind = alpb.index(lstr[i])
-            if ind < 0:
-                ind += len(alpb)
-        elif i % 7 == 5:
-            ind = alpb.index(lstr[i])
-            if ind >= len(alpb):
-                ind -= len(alpb)
-        elif i % 7 == 6:
-            ind = alpb.index(lstr[i])
-            if ind < 0:
-                ind += len(alpb)
-        ansls.append(ind)
-    ret = []
-    # print(ansls)
-    for i in range(len(ansls)):
-        ret.append(alpb[ansls[i]])
-    if key:
-        nout = ip
-        return(["".join(ret), nout])
-    else:
-        return(["".join(ret), nout])
 def splitn(x, num=1):
     try:
         int(num)
@@ -425,7 +349,7 @@ def repl(string, bf=None, af=None):
 def join_s(l):
     return(''.join(l))
 def mkmrna(string):
-    return('AUG' + exrnamino(shortamino(repl(string, ('o', 'u', 'b'), ('q', 'v', 'l')), True), True) + 'UGA')
+    return('AUG' + exrnamino(shortamino(repl(string, ('o', 'u', 'b'), ('q', 'v', 'l')), True), True) + 'UAG')
 def trlrna(string):
     return(repl(join_s([fixchr(c) for c in (shortamino(exrnamino(string)))]), ('q', 'v', 'l'), ('o', 'u', 'b')))
 def alpbnum(string, rev=False):
@@ -442,3 +366,79 @@ def alpbnum(string, rev=False):
         for i in s:
             n.append(al[int(i) - 1])
         return("".join(n))
+def fixstring(txt, cap=False):
+    if type(txt) == str:
+        n = []
+        al = [c for c in "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"]
+        # return("".join([fixchr(c, cap) for c in txt]))
+        for i in range(len(txt)):
+            if txt[i] in al:
+                n.append(fixchr(txt[i], cap))
+            else:
+                n.append(txt[i])
+        return("".join(n))
+    else:
+        return("Error")
+def remov(txt, char=" "):
+    return("".join([c for c in txt if c != char]))
+def elements(num, jp=False, en=False, rev=False):
+    elem = {
+        "1": ["H", "Na", "Sc", "Ga", "Nb"], 
+        "2": ["He", "Mg", "Ti", "Ge", "Mo"], 
+        "3": ["Li", "Al", "V", "As", "Tc"], 
+        "4": ["Be", "Si", "Cr", "Se", "Ru"], 
+        "5": ["B", "P", "Mn", "Br", "Rh"], 
+        "6": ["C", "S", "Fe", "Kr", "Pd"], 
+        "7": ["N", "Cl", "Co", "Rb", "Ag"], 
+        "8": ["O", "Ar", "Ni", "Sr", "Cd"], 
+        "9": ["F", "K", "Cu", "Y", "In"], 
+        "0": ["Ne", "Ca", "Zn", "Zr", "Sn"]
+    }
+    jpn = {
+        "ｱ": "H", "ｲ": "He", "ｳ": "Li", "ｴ": "Be", "ｵ": "B", 
+        "ｶ": "C", "ｷ": "N", "ｸ": "O", "ｹ": "F", "ｺ": "Ne", 
+        "ｻ": "Na", "ｼ": "Mg", "ｽ": "Al", "ｾ": "Si", "ｿ": "P", 
+        "ﾀ": "S", "ﾁ": "Cl", "ﾂ": "Ar", "ﾃ": "K", "ﾄ": "Ca", 
+        "ﾅ": "Sc", "ﾆ": "Ti", "ﾇ": "V", "ﾈ": "Cr", "ﾉ": "Mn", 
+        "ﾊ": "Fe", "ﾋ": "Co", "ﾌ": "Ni", "ﾍ": "Cu", "ﾎ": "Zn", 
+        "ﾏ": "Ga", "ﾐ": "Ge", "ﾑ": "As", "ﾒ": "Se", "ﾓ": "Br", 
+        "ﾔ": "Kr", "ﾕ": "Rb", "ﾖ": "Sr", 
+        "ﾗ": "Y", "ﾘ": "Zr", "ﾙ": "Nb", "ﾚ": "Mo", "ﾛ": "Tc", 
+        "ﾜ": "Ru", "ｦ": "Rh", "ﾝ": "Pd", "ﾞ": "Ag", "ﾟ": "Cd",
+        "ｯ": "In", "ｬ": "Sn", "ｭ": "Sb", "ｮ": "Te", "-": "I"
+    }
+    eng = {
+        "A": "H", "B": "He", "C": "Li", "D": "Be", "E": "B", 
+        "F": "C", "G": "N", "H": "O", "I": "F", "J": "Ne",
+        "K": "Na", "L": "Mg", "M": "Al", "N": "Si", "O": "P", 
+        "P": "S", "Q": "Cl", "R": "Ar", "S": "K", "T": "Ca", 
+        "U": "Sc", "V": "Ti", "W": "V", "X": "Cr", "Y": "Mn", 
+        "Z": "Fe", ",": "Co", ".": "Ni", "-": "Cu"
+    }
+    num = str(num)
+    if not rev:
+        if not jp:
+            import random
+            return("".join([elem[c][random.randrange(0, len(elem[c]))] for c in num]))
+        elif jp:
+            return("".join([jpn[c] for c in num]))
+        elif en:
+            return("".join(eng[c] for c in num))
+    elif rev:
+        txl = [c for c in num]
+        for i in range(len(txl)):
+            if txl[i] in [chr(c) for c in range(65, 91)]:
+                txl[i] = " " + txl[i]
+        txl = "".join(txl).split()
+        if jp:
+            for i in range(len(txl)):
+                txl[i] = ("".join([k for k, v in jpn.items() if v == txl[i]]))
+            return("".join(txl))
+        elif en:
+            for i in range(len(txl)):
+                txl[i] = ("".join([k for k, v in eng.items() if v == txl[i]]))
+            return("".join(txl))
+        else:
+            for i in range(len(txl)):
+                txl[i] = ("".join([k for k, v in elem.items() if txl[i] in v]))
+            return("".join(txl))
